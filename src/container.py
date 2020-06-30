1e637a881cd8ffff41c99d4d3234c968a0743069
spAm25 <39336091+spAm25@users.noreply.github.com>
2020-06-30 16:18:06 +0300

    Update container.py

--- src/container.py
+++ src/container.py
@@ -1,17 +1,22 @@
 # найти потеряную либу для шрифтов
 
+from fontTools import ttLib
+
 class FFMpegConf:
     def __init__(self):
         pass
 
 class Container:
-    def __init__(self,path):
-        self.__path = path
+    def __init__(self,path,attachments):
+        self.__paths = path
         self.__stream_list = []
         self.__attach_list = []
-    
+
     def compile_cmd(self,compiler = None, compiler_cong = None):
         return ""
+
+    def join(self,container):
+        pass
 
 class Stream:
     def __init__(self,path):
@@ -21,11 +26,26 @@ class Attach:
     def __init__(self,path):
         self.__path = path 
 
+    @property
+    def path(self):
+        return self.__path
+
 class FontAttach(Attach): 
     def __init__(self,path):
         super().__init__(path)
         self.__font_names = []
+        self.get_font_names()
 
     @property
     def font_names(self):
         return self.__font_names
+
+
+    def get_font_names(self):
+        font = ttLib.TTFont(self.path)
+        name = ""
+        for record in font['name'].names:
+            if record.nameID == 4:
+                name = str(record)
+                break
+        self.__font_names.append(name)
