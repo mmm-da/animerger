53cdbd1d8cfad8613ea651800658c2cff76b0401
spAm25 <39336091+spAm25@users.noreply.github.com>
2020-08-02 23:46:20 +0300

    attach mimetype fix

--- src/argument.py
+++ src/argument.py
@@ -9,20 +9,19 @@ class Argument:
         """Input section"""
         for input in container.container_list:
             result_str += ' -i "{0}"'.format(input)
-        for input in container.attach_list:
-            result_str += ' -i "{0}"'.format(input)
         """Stream sections"""
         for stream in container.stream_list:
             result_str += ' -map {0}:{1}'.format(stream.container_id,stream.stream_id)
             result_str += ' -c copy' 
             if stream.lang: result_str += ' -metadata language={0}'.format(stream.lang)
-        for font in container.attach_list:
-            result_str+= ' -attach "{0}"'.format(font)
+        for num,font in enumerate(container.attach_list):
+            result_str+= ' -attach "{0}" -c copy -metadata:s:t:{1} mimetype=application/x-truetype-font'.format(font,num)
         if new_name: result_str += ' "{0}"'.format(new_name)
         else: 
             new_name = list(os.path.splitext(container.container_list[0]))
             new_name[-1] = "_new"+new_name[-1]
             result_str += ' "{0}"'.format(str().join(new_name))
+        print("\n"+result_str+"\n")
         return result_str
         
 
