1021330df4ce8da79eb6598a7223f7feeb9d919f
spAm25 <39336091+spAm25@users.noreply.github.com>
2020-08-20 01:41:20 +0300

    change compiled container filename from ugly suffix to template name

--- src/argument.py
+++ src/argument.py
@@ -1,10 +1,11 @@
 import os
+import anitopy
 from iso639 import languages
 from ffmpeg import probe
 from container import MetaContainer,StreamTypes
 class Argument:
     @staticmethod
-    def compile_mkv(container,config:dict = {},new_name = None):
+    def compile_mkv(container,config:dict = {},new_name:str = None,name_template:str = None) -> str:
         result_str = "ffmpeg"
         """Input section"""
         for input in container.container_list:
@@ -20,12 +21,22 @@ class Argument:
             result_str+= ' -attach "{0}" -c copy -metadata:s:t:{1} mimetype=application/x-truetype-font'.format(font,num)
         if new_name: result_str += ' "{0}"'.format(new_name)
         else: 
-            new_name = list(os.path.splitext(container.container_list[0]))
-            new_name[-1] = "_new"+new_name[-1]
+            if name_template:
+                """TEMPLATE MAGIC"""
+                pass
+            else:
+                """ Default template - Title EpNum.mkv """
+            info = Argument.__parse_name(container.container_list[0])
+            new_name = "{0} {1}.mkv".format(info['title'],info['ep_num']) 
             result_str += ' "{0}"'.format(str().join(new_name))
         print("\n"+result_str+"\n")
         return result_str
-        
+
+    @staticmethod
+    def __parse_name(name:str) -> dict:
+        anitopy_options = dict({"parse_episode_number": True, "parse_episode_title": False, "parse_file_extension": False, "parse_release_group": False})
+        parse_result = anitopy.parse(name,options = anitopy_options)
+        return dict({'title': parse_result['anime_title'],'ep_num': parse_result['episode_number']})
 
 if __name__ == "__main__":
     print("Argument isn't executable module, but in Ka-52 you can start it.")
